706d61fb2724c8b342f9aeb08a74c8956abe6473
bushan1891 <bushan1891@gmail.com>
2016-01-07 12:24:37 -0800

    hibernate and junit

---
+++ HibernateTestProject/src/com/mitchell/Junit/JunitTest.java
@@ -0,0 +1,27 @@
+package com.mitchell.Junit;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class JunitTest {
+   
+	@Before
+	public void setUp() throws Exception {
+		
+	}
+	
+	@After
+	public void tearDown() throws Exception {
+		
+	}
+	
+	
+	@Test
+	public void test() {
+		
+	}
+
+}
